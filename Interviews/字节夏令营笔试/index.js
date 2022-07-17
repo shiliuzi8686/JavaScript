2409b2f4d0ed5fc8cd4156417b1ea4a55d687235
shiliuzi8686 <2931857501@qq.com>
2022-07-18 01:27:57 +0800

    22.7.18笔试

---
+++ Interviews/字节夏令营笔试/index.js
@@ -0,0 +1,21 @@
+const {log} = console
+
+// var name = 'window'
+// var bar = { name:'bar' }
+// var foo = {
+//     name:'foo',
+//     solve(){
+//         return () => console.log(this.name)
+//     }
+// }
+// foo.solve.call(bar)()
+// foo.solve().call(bar)
+
+// var f = function(){
+//     log('1')
+// }
+// function f(){
+//     log('2')
+// }
+// f()
+
